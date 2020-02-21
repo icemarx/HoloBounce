5a0b1738d319bef41d8d96a81afb847213ce2c2f
aekai <ak4352@student.uni-lj.si>
2020-02-21 12:31:07 +0100

    Basic toss - click not working yet

---
+++ Bounce/Assets/Scripts/BallControlls.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BallControlls : MonoBehaviour
+{
+
+    public float forceAmount = 1f;
+    public Rigidbody rb;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnMouseDown()
+    {
+        Vector3 vec = this.transform.position - Camera.main.transform.position;
+        vec = vec.normalized * forceAmount;
+        rb.AddForce(vec, ForceMode.Impulse);
+        rb.useGravity = true;
+    }
+
+}
