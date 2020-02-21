c13af0219b7695529fe039adc554ba01347c255c
Matjaz Ciglic <matjaz.ciglic.jst@gmail.com>
2020-02-21 13:31:50 +0100

    Add Player script with collision based pickup mode

---
+++ Bounce/Assets/Scripts/Player.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    public bool isHolding;
+
+    public Transform holdTransform;
+    
+    void Start() {
+        
+    }
+
+    private void OnTriggerEnter(Collider other) {
+        // handle collision
+        if (other.CompareTag("Ball")) {
+            // Debug.Log("Ball hit");
+            other.transform.SetParent(transform);
+            other.transform.SetPositionAndRotation(holdTransform.position, holdTransform.localRotation);
+            isHolding = true;
+        }
+    }
+}
