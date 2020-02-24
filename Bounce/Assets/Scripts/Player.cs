723a547ada574526918c7b0e58584bc5b8577c7e
Matjaz Ciglic <matjaz.ciglic.jst@gmail.com>
2020-02-24 12:02:18 +0100

    Make pick-up a method, add out-of-map pick-up
    Needs to be tweaked and tested

--- Bounce/Assets/Scripts/Player.cs
+++ Bounce/Assets/Scripts/Player.cs
@@ -5,20 +5,34 @@ using UnityEngine;
 public class Player : MonoBehaviour
 {
     public bool isHolding;
+    public float maxDistance = 20;
 
     public Transform holdTransform;
+    public GameObject ball;
     
     void Start() {
         
+    }
+
+    void Update() {
+        // check distance from ball
+        float distance = (transform.position - ball.transform.position).magnitude;
+        if (distance >= maxDistance) {
+            PickUp(ball.transform);
+        }
     }
 
     private void OnTriggerEnter(Collider other) {
         // handle collision
         if (other.CompareTag("Ball")) {
             // Debug.Log("Ball hit");
-            other.transform.SetParent(transform);
-            other.transform.SetPositionAndRotation(holdTransform.position, holdTransform.localRotation);
-            isHolding = true;
+            PickUp(other.transform);
         }
+    }
+
+    public void PickUp(Transform t) {
+        t.SetParent(transform);
+        t.SetPositionAndRotation(holdTransform.position, holdTransform.localRotation);
+        isHolding = true;
     }
 }
