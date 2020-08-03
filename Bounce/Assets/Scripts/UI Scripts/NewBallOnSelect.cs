c2e573c608071bac406da1bac01131e2d5293dd4
Matjaz Ciglic <matjaz.ciglic.jst@gmail.com>
2020-08-04 00:23:04 +0200

    Add GameManager to NewBallOnSelect class

--- Bounce/Assets/Scripts/UI Scripts/NewBallOnSelect.cs
+++ Bounce/Assets/Scripts/UI Scripts/NewBallOnSelect.cs
@@ -3,14 +3,26 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class NewBallOnSelect : MonoBehaviour {
+    public GameManager gameManager;
+
+    private void Awake() {
+        /*
+         * NOTE: this piece of code should be present whenever an Object should require
+         * the GameManager at some point, in case it is not added in the editor.
+         */
+        if(gameManager == null) {
+            gameManager = GameObject.Find("GameManagerObject").GetComponent<GameManager>();
+        }
+    }
+
     public void OnSelect() {
         // Destroy old ball
         GameObject ball = GameManager.GetFirstBall();
         GameManager.RemoveBall(ball);
         Destroy(ball);
-
+        
         // create new ball
-        GameObject newball = GameManager.CreateBall();
+        GameObject newball = gameManager.CreateBall();
         newball.SetActive(true);
         
         // pass it to player
