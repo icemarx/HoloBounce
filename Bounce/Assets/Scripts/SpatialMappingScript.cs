a0a3d6c524fb364701c680a853d4e7c1980ada91
Matjaz Ciglic <matjaz.ciglic.jst@gmail.com>
2020-09-11 15:02:04 +0200

    Limit environment scanning time
    Scanning should now be limited to only first 10 seconds (can be changed) after start of the game, after which scanning is stopped.
    This should save resources for longer runtimes of the game, although should require scanning before the game starts or rescanning when the game is active.

---
+++ Bounce/Assets/Scripts/SpatialMappingScript.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.XR.WSA;
+
+public class SpatialMappingScript : MonoBehaviour
+{
+    [SerializeField]
+    private float SCANNING_TIME = 10f;
+    private SpatialMappingCollider smCollider;
+    private SpatialMappingRenderer smRenderer;
+
+    // Start is called before the first frame update
+    void Start() {
+        smCollider = gameObject.GetComponentInParent<SpatialMappingCollider>();
+        smRenderer = gameObject.GetComponentInParent<SpatialMappingRenderer>();
+
+        StartCoroutine("ScannEnvironment");
+
+    }
+
+    IEnumerator ScannEnvironment() {
+        // start scanning environment
+        smCollider.freezeUpdates = true;
+        smRenderer.freezeUpdates = true;
+        // Debug.Log("Started Scanning");
+
+        // wait
+        yield return new WaitForSeconds(SCANNING_TIME);
+
+        // end scanning
+        smCollider.freezeUpdates = false;
+        smRenderer.freezeUpdates = false;
+        // Debug.Log("Stopped Scanning");
+    }
+}
