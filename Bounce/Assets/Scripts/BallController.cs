b690aa51023992d09d8d1cb0d6e3263f1fc75a9f
Aekai <ak4352@student.uni-lj.si>
2020-11-03 20:24:54 +0100

    Add a bounce sfx and add code to play it on bounce

--- Bounce/Assets/Scripts/BallController.cs
+++ Bounce/Assets/Scripts/BallController.cs
@@ -8,8 +8,13 @@ public class BallController : MonoBehaviour
     public float forceAmount = 1f;
     private Rigidbody rb;
 
+    [SerializeField]
+    private AudioSource bounceSound; // Reference to the bounce sound AudioSource on the ball
+    private float bounce_volume; // Initial volume of the bounce sound audiosource
+
     private void Awake() {
         rb = GetComponent<Rigidbody>();
+        bounce_volume = bounceSound.volume;
     }
 
     /// <summary>
@@ -24,5 +29,23 @@ public class BallController : MonoBehaviour
 
         // apply gravity
         rb.useGravity = true;
+    }
+
+    /// <summary>
+    /// Called when the ball enters collision with another collider.
+    /// </summary>
+    private void OnCollisionEnter(Collision collision)
+    {
+        // if the ball does not collide with the player
+        if (!collision.other.CompareTag("Player"))
+        {
+            // change pitch slightly based on velocity
+            bounceSound.pitch = 1f + Mathf.Min(0.2f, 2f*rb.velocity.magnitude/100f);
+            bounceSound.volume = bounce_volume + Mathf.Min(0.2f, 2f * rb.velocity.magnitude / 100f);
+            Debug.Log(rb.velocity.magnitude);
+
+            // play the bounce sound effect
+            bounceSound.Play();
+        }
     }
 }
