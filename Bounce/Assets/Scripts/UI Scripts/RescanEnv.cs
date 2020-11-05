2e787e9a00c742651a23f3181a3c6beb24bca88c
Matjaz Ciglic <matjaz.ciglic.jst@gmail.com>
2020-11-06 00:19:15 +0100

    Implement Rescan UI Element

---
+++ Bounce/Assets/Scripts/UI Scripts/RescanEnv.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RescanEnv : MonoBehaviour {
+    public GameManager gameManager;
+    public SpatialMappingScript spatialMappingScript;
+
+    private void Awake() {
+        /*
+         * NOTE: this piece of code should be present whenever an Object should require
+         * the GameManager at some point, in case it is not added in the editor.
+         */
+        if (gameManager == null) {
+            gameManager = GameObject.Find("GameManagerObject").GetComponent<GameManager>();
+        }
+    }
+    
+    /// <summary>
+    /// Call the <c>Rescan</c> method of the <c>SpatialMappingScript</c>.
+    /// <see cref="SpatialMappingScript.Rescan"/>
+    /// </summary>
+    public void OnSelect() {
+        spatialMappingScript.Rescan();
+    }
+}
