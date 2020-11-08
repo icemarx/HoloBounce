80e358a68854f0cc8c16426c8cbf0eb6d49824ed
Aekai <ak4352@student.uni-lj.si>
2020-11-08 16:28:57 +0100

    Add mute ui box and rolling sfx

--- Bounce/Assets/Scripts/BallController.cs
+++ Bounce/Assets/Scripts/BallController.cs
@@ -12,9 +12,32 @@ public class BallController : MonoBehaviour
     private AudioSource bounceSound; // Reference to the bounce sound AudioSource on the ball
     private float bounce_volume; // Initial volume of the bounce sound audiosource
 
+    [SerializeField]
+    private AudioSource rollSound; // Reference to the roll sound AudioSource on the ball
+    private float roll_volume; // Initial volume of the roll sound audiosource
+
+    private bool isRolling; // If true, rolling sound effect should play
+
     private void Awake() {
         rb = GetComponent<Rigidbody>();
         bounce_volume = bounceSound.volume;
+        roll_volume = rollSound.volume;
+    }
+
+    private void Update()
+    {
+        // if the ball is not moving (or really slow) stop playing the rolling sound
+         if (GameManager.muteSound || rb.velocity.magnitude < 0.01f)
+        {
+            rollSound.Stop();
+            isRolling = false;
+        }
+         else
+        {
+            // change pitch and volume slightly based on velocity
+            rollSound.pitch = 0.8f + Mathf.Min(0.3f, 2f * rb.velocity.magnitude / 100f);
+            rollSound.volume = roll_volume + Mathf.Min(0.4f, 2f * rb.velocity.magnitude / 100f);
+        }
     }
 
     /// <summary>
@@ -37,15 +60,46 @@ public class BallController : MonoBehaviour
     private void OnCollisionEnter(Collision collision)
     {
         // if the ball does not collide with the player
-        if (!collision.other.CompareTag("Player"))
+        if (!collision.collider.CompareTag("Player"))
         {
             // change pitch slightly based on velocity
             bounceSound.pitch = 1f + Mathf.Min(0.2f, 2f*rb.velocity.magnitude/100f);
             bounceSound.volume = bounce_volume + Mathf.Min(0.2f, 2f * rb.velocity.magnitude / 100f);
-            // Debug.Log(rb.velocity.magnitude);
 
             // play the bounce sound effect
-            bounceSound.Play();
+            if (!GameManager.muteSound)
+                bounceSound.Play();
+        }
+    }
+
+    /// <summary>
+    /// Called when the ball stays in collision with an object e.g. rolling on the floor.
+    /// </summary>
+    private void OnCollisionStay(Collision collision)
+    {
+        // if the ball does not collide with the player and is moving
+        if (!collision.collider.CompareTag("Player") && rb.velocity.magnitude > 0.1f)
+        {
+            // play the rolling sound
+            if (!isRolling && !GameManager.muteSound)
+            {
+                rollSound.Play();
+                isRolling = true;
+            }
+        }
+    }
+
+
+    /// <summary>
+    /// Called when the ball exits collision with an object e.g. stops rolling on the floor.
+    /// </summary>
+    private void OnCollisionExit(Collision collision)
+    {
+        // stop the rolling sound
+        if (isRolling)
+        {
+            rollSound.Stop();
+            isRolling = false;
         }
     }
 }
---
+++ Bounce/Assets/Scripts/UI Scripts/ToggleMute.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ToggleMute : MonoBehaviour {
+    public GameManager gameManager;
+
+    private void Awake() {
+        /*
+         * NOTE: this piece of code should be present whenever an Object should require
+         * the GameManager at some point, in case it is not added in the editor.
+         */
+        if (gameManager == null) {
+            gameManager = GameObject.Find("GameManagerObject").GetComponent<GameManager>();
+        }
+    }
+
+    /// <summary>
+    /// Toggles whether the sound should be muted or not.
+    /// <see cref="GameManager.muteSound"/>
+    /// </summary>
+    public void OnSelect() {
+        GameManager.muteSound = !(GameManager.muteSound);
+    }
+}
