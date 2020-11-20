ed9c8f4866203dde430c68279b9ea95d3806938f
Matjaz Ciglic <matjaz.ciglic.jst@gmail.com>
2020-11-20 18:23:41 +0100

    Add Toggle Credits UI Element
    It is very basic but should work for now, while the UI is in it's current state.

---
+++ Bounce/Assets/Scripts/UI Scripts/ToggleCredits.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ToggleCredits : MonoBehaviour
+{
+    public GameObject creditsText;
+
+    private void Start() {
+        // if no credits are available, destroy this UI Element
+        if (creditsText == null)
+            Destroy(this);
+    }
+
+    /// <summary>
+    /// When clicked, it sets the <c>creditsText</c> active state to the opposite
+    /// of the previous state. This effectively works as a toggle.
+    /// </summary>
+    public void OnSelect() {
+        creditsText.SetActive(!creditsText.activeSelf);
+    }
+}
