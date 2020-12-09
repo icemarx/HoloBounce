10465adf87fbccc97fd691e62d0ee0a0bd63a417
oskarcokl <oskar.cokl@gmail.com>
2020-12-09 16:01:33 +0100

    Added neccessary global variables to the MeshDeformer class.

--- Bounce/Assets/Scripts/MeshDeformer.cs
+++ Bounce/Assets/Scripts/MeshDeformer.cs
@@ -2,9 +2,30 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+
+// This ensures there is a MeshFilter component present on the object
+[RequireComponent(typeof(MeshFilter))]
 public class MeshDeformer : MonoBehaviour
 {
-    public void AddDeformingForce(Vector3[] points, float force) {
+    // How fast the deforming mesh returns to it's original position.
+    public float springForce = 20f;
 
+    // Factor of "calming down" when springin back.
+    public float damping = 5f;
+
+    // We need this so that the deformation is the same regardles of
+    // scale of the mesh.
+    float uniformScale = 1f;
+
+    // The mesh we will be deforming.
+    Mesh deformingMesh;
+
+    // originalVertices are needed so that the ball can spring back
+    // to it's previous shape.
+    Vector3[] originalVertices, displacedVertices, vertexVelocities;
+
+
+    public void AddDeformingForce(Vector3[] points, float force) {
+        Debug.Log("Hiipidy dippity I'm deforming the ball");
     }
 }
